2008ad48dd3224515968aa27b04372619748d89c
pinkdaniel@web.de <pinkdaniel@web.de>
2020-08-31 21:32:02 +0200

    move calculateChanges & numNeighbours inside class. add semicolons

--- game.js
+++ game.js
@@ -1,96 +1,95 @@
 class Game {
     constructor(width) {
-        this.width = width
+        this.width = width;
         this.size = 50;
         this.running = true;
-        this.reset()
+        this.reset();
     }
 
     update() {
-        const changes = calculateChanges(this.field)
+        const changes = this.calculateChanges();
         if (this.running) {
             changes.map(c => {
-                this.field[c.y][c.x] = c.change
+                this.field[c.y][c.x] = c.change;
             })
         }
     }
 
     run(){
-        this.running = !this.running
+        this.running = !this.running;
     }
 
     reset() {
         this.field = [];
         for (let y = 0; y < this.size - 3; y++) {
-            this.field.push([])
+            this.field.push([]);
             for (let x = 0; x < this.size - 3; x++) {
-                this.field[y].push(random([true, false]))
+                this.field[y].push(random([true, false]));
             }
         }
     }
 
     draw = () => {
-        let w = this.width / this.size
-        let h = this.width / this.size
+        let w = this.width / this.size;
+        let h = this.width / this.size;
 
-        stroke(0)
-        strokeWeight(0.1)
+        stroke(0);
+        strokeWeight(0.1);
 
         for (let y = 1; y < this.field.length - 1; y++) {
             for (let x = 1; x < this.field[y].length - 1; x++) {
-
                 if (this.field[y][x]) {
-                    fill(0)
+                    fill(0);
                 } else {
-                    fill(255)
+                    fill(255);
                 }
-                rect(x + x * w, y + y * h, w, h)
+                rect(x + x * w, y + y * h, w, h);
             }
         }
     }
-}
 
-calculateChanges = (f) => {
-    let changes = []
-    for (let y = 0; y < f.length; y++) {
-        for (let x = 0; x < f[y].length; x++) {
-            let nn = numNeighbours(f, x, y)
-            stroke(255, 0, 0)
-            let change;
-            if (f[y][x]) { // alive
-                if (nn < 2) {
-                    change = false
+    calculateChanges = () => {
+        let changes = [];
+        for (let y = 0; y < this.field.length; y++) {
+            for (let x = 0; x < this.field[y].length; x++) {
+                let nn = this.numNeighbours(this.field, x, y)
+                stroke(255, 0, 0);
+                let change;
+                if (this.field[y][x]) { // alive
+                    if (nn < 2) {
+                        change = false;
+                    }
+                    if (nn > 3) {
+                        change = false;
+                    }
+                    if (nn === 2 || nn === 3) {
+                        change = true;
+                    }
+                } else { // dead 
+                    if (nn === 3) {
+                        change = true;
+                    }
                 }
-                if (nn > 3) {
-                    change = false
-                }
-                if (nn === 2 || nn === 3) {
-                    change = true
-                }
-            } else { // dead 
-                if (nn === 3) {
-                    change = true
-                }
-            }
-
-            changes.push({ x: x, y: y, change: change })
-        }
-    }
-    return changes
-}
-
-numNeighbours = (f, x, y) => {
-    let count = 0
-    for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-            if (i === 0 && j === 0) {
-                continue;
-            }
-
-            if (f[y + i] && f[y + i][x + j]) {
-                count += 1;
+                changes.push({ x: x, y: y, change: change });
             }
         }
+        return changes;
     }
-    return count
+    
+    numNeighbours = (f, x, y) => {
+        let count = 0;
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (i === 0 && j === 0) {
+                    continue;
+                }
+    
+                if (f[y + i] && f[y + i][x + j]) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
 }
+
--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 let game;
 
 setup = () => {
-    const width = initCanvas()
-    game = new Game(width)
-    frameRate(15)
+    const width = initCanvas();
+    game = new Game(width);
+    frameRate(15);
 }
 
 draw = () => {
     background(220);
-    game.update()
-    game.draw()
+    game.update();
+    game.draw();
 }
 
 
 changeFrameRate = (v) => {
-    const currentFrameRate = Math.floor(frameRate())
-    const newFrameRate = (currentFrameRate + v > 0 && currentFrameRate + v < 60) ? currentFrameRate + v : currentFrameRate
-    frameRate(newFrameRate)
+    const currentFrameRate = Math.floor(frameRate());
+    const newFrameRate = (currentFrameRate + v > 0 && currentFrameRate + v < 60) ? currentFrameRate + v : currentFrameRate;
+    frameRate(newFrameRate);
 }
 
 initCanvas = () => {
     var canvasDiv = document.getElementById('p5canvas');
-    var width = canvasDiv.offsetWidth
-    canvasW = width
-    canvasH = width
-    canvas = createCanvas(canvasW, canvasH)
+    var width = canvasDiv.offsetWidth;
+    canvasW = width;
+    canvasH = width;
+    canvas = createCanvas(canvasW, canvasH);
     canvas.parent('p5canvas');
     return canvasW;
 }
